31d386e29e6c728dd708627b3ce212228381833d
Ton Schoots <ton@maiastra.com>
2016-10-27 17:35:38 +0200

    initial server

---
+++ web/web_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os/exec"
+    //"os"
+)
+
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there!\n\n")
+    
+    cmd := exec.Command("java",
+                        "-version")
+	fmt.Println(cmd.Args)
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	
+	cmd.Stdout = w
+	cmd.Stderr = w
+	
+	//go io.Copy(log., os.Stderr)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+		//return false, err
+	}
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
