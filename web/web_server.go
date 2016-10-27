9ae7d4fe19a9e635a0a563c237ff2a1fb5981c09
root <root@2mdev11.blackducksoftware.com>
2016-10-27 15:49:15 -0400

    test git access

--- web/web_server.go
+++ web/web_server.go
@@ -31,5 +31,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":80", nil)
 }
